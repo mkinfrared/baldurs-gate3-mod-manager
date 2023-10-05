0d623346d28aaf2ecff43e68968ed17b8a259bda
Mikhail Korotkov <mkinfrared@gmail.com>
2023-10-05 16:19:37 +0400

    feat: add installed mods ui

--- src/renderer/features/modFile/ui/EmptyList/EmptyList.type.ts
+++ src/renderer/features/modFile/ui/EmptyList/EmptyList.type.ts
@@ -1,9 +1,6 @@
-import { ChangeEventHandler } from "react";
-
 export type EmptyListProps = {
   /**
    * a string that will be applied as a css class to parent element
    */
   className?: string;
-  onFilesSelect: ChangeEventHandler<HTMLInputElement>;
 };
--- src/renderer/features/modSettingsFile/ui/InstalledMod/InstalledMod.type.ts
+++ src/renderer/features/modSettingsFile/ui/InstalledMod/InstalledMod.type.ts
@@ -1,9 +1,6 @@
-import { RouterOutputs } from "main/shared/types";
-
 export type InstalledModProps = {
   /**
    * a string that will be applied as a css class to parent element
    */
   className?: string;
-  mod: RouterOutputs["mod"]["getInstalledMods"][number];
 };
