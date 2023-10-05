18c6d1150a7f52ab5b21cacb4705c70f0316982c
Mikhail Korotkov <mkinfrared@gmail.com>
2023-10-06 03:34:04 +0400

    feat: toggle active mod state

---
+++ src/main/features/mod/toggleActiveModHandler/toggleActiveModHandler.ts
@@ -0,0 +1,15 @@
+import { activateMod, deactivateMod } from "main/entities/mod";
+
+import { isActiveMod } from "./lib/helpers";
+
+const toggleActiveModHandler = async (modUUID: string) => {
+  const isActive = await isActiveMod(modUUID);
+
+  if (isActive) {
+    await deactivateMod(modUUID);
+  } else {
+    await activateMod(modUUID);
+  }
+};
+
+export { toggleActiveModHandler };
