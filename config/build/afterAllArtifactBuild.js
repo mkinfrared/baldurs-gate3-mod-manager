12619c8c2358a9151ba0bdfc2231279bc01f47b0
Mikhail Korotkov <mkinfrared@gmail.com>
2023-10-07 21:49:16 +0400

    fix: remove excessive dmg sign
    * update README on installation process

--- config/build/afterAllArtifactBuild.js
+++ config/build/afterAllArtifactBuild.js
@@ -1,27 +1,11 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-var-requires,no-console */
 const fs = require("fs");
 const path = require("path");
 const { promisify } = require("util");
 const exec = promisify(require("child_process").exec);
 
-const afterPack = async (context) => {
-  const appName = '"Baldur\'s Gate 3 Mod Manager.app"'; // added double-quotes
-  const rootDir = path.resolve(__dirname, "../..");
-  const buildDir = path.join(rootDir, "./release/build");
-  const files = fs.readdirSync(path.join(buildDir));
-  const dmgFiles = files.filter((file) => /\.dmg$/.test(file));
-
-  for (const dmgFile of dmgFiles) {
-    try {
-      const filePath = path.join(buildDir, dmgFile);
-
-      await exec(`codesign -s "baldurs-gate3-mod-manager" --deep ${filePath}`);
-
-      await exec(`codesign -vvv ${filePath}`);
-    } catch (e) {
-      console.error(e);
-    }
-  }
+const afterAllArtifactBuild = (context) => {
+  console.log("afterAllArtifactBuild");
 };
 
-exports.default = afterPack;
+exports.default = afterAllArtifactBuild;
