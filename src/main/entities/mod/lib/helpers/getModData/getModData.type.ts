5af8053035b6f65973592d2f1cb5e1929ac7fcfd
Mikhail Korotkov <mkinfrared@gmail.com>
2023-10-08 14:30:36 -0700

    chore(release): 1.2.1 (#45)
    * fix: incorrect mod type check (#43)

--- src/main/entities/mod/lib/helpers/getModData/getModData.type.ts
+++ src/main/entities/mod/lib/helpers/getModData/getModData.type.ts
@@ -10,6 +10,23 @@ type ModInfoV1 = {
   Group: string;
 };
 
+// {
+//   Mods: [
+//     {
+//       Author: 'Koriik',
+//       Name: 'Dice Set Expansion',
+//       Folder: 'DiceSkin01',
+//       Version: null,
+//       Description: 'Additional dice skin colours.',
+//       UUID: 'ae620a20-a88b-433e-9d1a-bdd18317cc79',
+//       Created: '2023-08-12T20:42:57.8704626+01:00',
+//       Dependencies: [],
+//       Group: '932a9d8b-cb56-420f-89a8-90b6dd72d1cb'
+//     }
+//   ],
+//       MD5: '027a64cd744f5c71a1786a3ad996abd0'
+// }
+
 type ModInfoV2 = {
   folderName: string;
   modName: string;
@@ -32,13 +49,13 @@ export type Mod = ModInfoV1 | ModInfoV2;
 export type ModData = ModDataV1 | ModDataV2;
 
 const isModInfoV1 = (value: any): value is ModInfoV1 => {
-  const props = ["UUID", "modName", "version", "MD5"];
+  const props = ["Author", "Name", "Folder", "UUID"];
 
   return props.every((prop) => Object.hasOwn(value, prop));
 };
 
 const isModInfoV2 = (value: any): value is ModInfoV2 => {
-  const props = ["UUID", "Version", "Author", "Name"];
+  const props = ["folderName", "modName", "MD5", "version", "UUID"];
 
   return props.every((prop) => Object.hasOwn(value, prop));
 };
