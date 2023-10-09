af81d011b210cc1f3f7f8013456e41c8d92c1214
Mikhail Korotkov <mkinfrared@gmail.com>
2023-10-09 13:25:37 +0400

    revert: rar and 7z support
    * rolling back these changes due to libarchive doesn't work in
    production build
    
    * This reverts commit 5e82fe10e1bca652353b08253aa9ddb8e420b2cd.

--- src/main/entities/mod/lib/helpers/getModData/getModData.ts
+++ src/main/entities/mod/lib/helpers/getModData/getModData.ts
@@ -1,9 +1,46 @@
+/* eslint-disable new-cap */
+import StreamZip from "node-stream-zip";
+
+import { isJson, isPak } from "renderer/shared/lib/helpers/fileExtension";
 import { memoizeTtl } from "renderer/shared/lib/helpers/memoizeTtl";
 
-import { getModDataFromArchive } from "./lib";
+import { ModData, isModData } from "./getModData.type";
+
+const getModDataFromZip = async (zipFilePath: string) => {
+  const zip = new StreamZip.async({ file: zipFilePath, storeEntries: true });
+  const entries = await zip.entries();
+
+  let modData: ModData | undefined;
+
+  const pakFiles: string[] = [];
+
+  for (const entry of Object.values(entries)) {
+    if (!entry.isFile) {
+      continue;
+    }
+
+    if (isJson(entry.name)) {
+      const entryData = await zip.entryData(entry.name);
+      const contents = entryData.toString();
+      const data = JSON.parse(contents);
+
+      if (isModData(data)) {
+        modData = data;
+      }
+    }
+
+    if (isPak(entry.name)) {
+      pakFiles.push(entry.name);
+    }
+  }
+
+  await zip.close();
+
+  return { modData, pakFiles };
+};
 
 const getModData = async (filePath: string) => {
-  const { modData, pakFiles } = await getModDataFromArchive(filePath);
+  const { modData, pakFiles } = await getModDataFromZip(filePath);
 
   return { modData, filePath, pakFiles };
 };
