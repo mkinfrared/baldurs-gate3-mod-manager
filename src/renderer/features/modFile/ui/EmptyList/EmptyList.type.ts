cbb98e03ef353f279e9df4a6609fba306ad9fd87
Mikhail Korotkov <mkinfrared@gmail.com>
2023-10-09 04:01:36 -0700

    revert: rar and 7z support (#48)

--- src/renderer/features/modFile/ui/EmptyList/EmptyList.type.ts
+++ src/renderer/features/modFile/ui/EmptyList/EmptyList.type.ts
@@ -1,7 +1,6 @@
 import { ChangeEventHandler } from "react";
 
 export type EmptyListProps = {
-  acceptedFileTypes: string[];
   /**
    * a string that will be applied as a css class to parent element
    */
